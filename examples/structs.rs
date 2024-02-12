d2fa558bba17c382241cbe4397edb7cb4a6b46d7
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-02-13 01:23:56 +0200

    wip

--- examples/structs.rs
+++ examples/structs.rs
@@ -3,6 +3,7 @@ use invade::Invade;
 #[derive(Debug, Invade)]
 struct Counter {
     count: u32,
+    title: String,
 }
 
 impl Counter {
@@ -12,7 +13,7 @@ impl Counter {
 }
 
 fn main() {
-    let mut counter = Counter { count: 69 };
+    let mut counter = Counter { count: 69, title: "Counter".to_string()};
 
     println!("count: {:?}", counter.invade().get::<u32>("count"));
     println!("count: {:?}", counter.invade_get::<u32>("count"));
@@ -25,4 +26,6 @@ fn main() {
     counter.invade_call("inc");
 
     println!("count: {}", counter.count);
+
+    println!("title: {:?}", counter.invade_get::<String>("title"));
 }
--- invade_derive/src/lib.rs
+++ invade_derive/src/lib.rs
@@ -11,23 +11,45 @@ pub fn derive_invade(input: TokenStream) -> TokenStream {
 
 fn impl_invade(input: &syn::DeriveInput) -> TokenStream {
     let name = &input.ident;
+    let fields = match &input.data {
+        syn::Data::Struct(s) => match &s.fields {
+            syn::Fields::Named(f) => f.named.to_owned(),
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    };
+
+   
+    let mut field_items = proc_macro2::TokenStream::new();
+
+    for field in fields {
+        let field_name = field.ident.unwrap(); // Field name
+        let field_str = field_name.to_string();
+        let field_type = field.ty; // Field type
+
+        let token = quote!(
+            invade::Field {
+                name: #field_str.to_string(),
+                get_ptr: Some(|s| -> &dyn std::any::Any {
+                    &s.#field_name as &dyn std::any::Any
+                }),
+                set_ptr: Some(|s, v| {
+                    if let Some(v) = v.downcast_ref::<#field_type>() {
+                        s.#field_name = (*v).clone();
+                    }
+                }),
+            },
+        );
+        field_items.extend(token);
+    }
+   
     let gen = quote! {
         impl #name {
             fn invade(&mut self) -> invade::Invaded<&mut Self> {
                 invade::Invaded::new(
                     self,
                     vec![
-                        invade::Field {
-                            name: "count".to_string(),
-                            get_ptr: Some(|s| -> &dyn std::any::Any {
-                                &s.count as &dyn std::any::Any
-                            }),
-                            set_ptr: Some(|s, v| {
-                                if let Some(v) = v.downcast_ref::<u32>() {
-                                    s.count = *v;
-                                }
-                            }),
-                        }
+                        #field_items
                     ],
                     vec![
                         invade::Method {
